3051cdb857e96d0828bf9b26babaa3a5df9eebcf
uaaeu <yurtmusa@gmail.com>
2020-07-14 16:15:51 +0300

    Learn About SVG in D3

--- d3.js
+++ d3.js
@@ -77,3 +77,11 @@ d3.select("body")
   .append("div")
   .attr("class", "bar")
   .style("height", (d) => d * 10 + "px");
+
+//Learn About SVG in D3
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+const w = 500;
+const h = 100;
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
