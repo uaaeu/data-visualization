1c95afee8edf7a99391da1dd338337df15c2a160
uaaeu <yurtmusa@gmail.com>
2020-07-14 15:57:46 +0300

    Work with Dynamic Data in D3

--- d3.js
+++ d3.js
@@ -13,3 +13,13 @@ d3.select("body")
   .enter()
   .append("h2")
   .text("New Title");
+
+//Work with Dynamic Data in D3
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body")
+  .selectAll("h2")
+  .data(dataset)
+  .enter()
+  .append("h2")
+  .text((d) => d + " USD");
