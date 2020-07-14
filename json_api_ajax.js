0185f41eaa397457a025d90b35ad9fd2e18b0d70
uaaeu <yurtmusa@gmail.com>
2020-07-14 22:02:50 +0300

    Get JSON with the JavaScript XMLHttpRequest Method

--- json_api_ajax.js
+++ json_api_ajax.js
@@ -1,9 +1,23 @@
 //Handle Click Events with JavaScript using the onclick property
 document.addEventListener("DOMContentLoaded", function () {
-  
-    document.getElementById("getMessage").onclick = function () {
-    
+  document.getElementById("getMessage").onclick = function () {
     //Change Text with click Events
-    document.getElementsByClassName("message")[0].textContent="Here is the message";
+    document.getElementsByClassName("message")[0].textContent =
+      "Here is the message";
+  };
+});
+
+//Get JSON with the JavaScript XMLHttpRequest Method
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("getMessage").onclick = function () {
+    const req = new XMLHttpRequest();
+    req.open("GET", "/json/cats.json", true);
+    req.send();
+    req.onload = function () {
+      const json = JSON.parse(req.responseText);
+      document.getElementsByClassName("message")[0].innerHTML = JSON.stringify(
+        json
+      );
+    };
   };
 });
