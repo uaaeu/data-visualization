fe00409b51741b5e1e78d7a26b2908c3df0182a3
uaaeu <yurtmusa@gmail.com>
2020-07-14 18:00:57 +0300

    d3.max d3.min

--- d3.js
+++ d3.js
@@ -423,3 +423,14 @@ scale.domain([250, 500]).range([10, 150]);
 
 const output = scale(50);
 d3.select("body").append("h2").text(output);
+
+//Use the d3.max and d3.min Functions to Find Minimum and Maximum Values in a Dataset
+const positionData = [
+  [1, 7, -4],
+  [6, 3, 8],
+  [2, 9, 3],
+];
+
+const output = d3.max(positionData, (d) => d[2]);
+
+d3.select("body").append("h2").text(output);
