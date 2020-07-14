01dc9808c7877d35c934cc6903b1975a0621b136
uaaeu <yurtmusa@gmail.com>
2020-07-14 17:18:30 +0300

    Create a Scatterplot with SVG Circles

--- d3.js
+++ d3.js
@@ -325,3 +325,24 @@ svg
   .text((d) => d)
   .attr("x", (d, i) => i * 30)
   .attr("y", (d, i) => h - (d * 3 + 3));
+
+//Create a Scatterplot with SVG Circles
+const dataset = [
+  [34, 78],
+  [109, 280],
+  [310, 120],
+  [79, 411],
+  [420, 220],
+  [233, 145],
+  [333, 96],
+  [222, 333],
+  [78, 320],
+  [21, 123],
+];
+
+const w = 500;
+const h = 500;
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+svg.selectAll("circle").data(dataset).enter().append("circle");
