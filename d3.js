a3bbe788710352237b01bded67debce1f8cd44ac
uaaeu <yurtmusa@gmail.com>
2020-07-14 16:10:16 +0300

    Change the Presentation of a Bar Chart

--- d3.js
+++ d3.js
@@ -66,3 +66,14 @@ d3.select("body")
   .append("div")
   .attr("class", "bar")
   .style("height", (d) => d + "px");
+
+//Change the Presentation of a Bar Chart
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", (d) => d * 10 + "px");
