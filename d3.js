5c6fdd62d460bb0af00cbff2ca1814e2de92a7ea
uaaeu <yurtmusa@gmail.com>
2020-07-14 15:59:23 +0300

    Add Inline Styling to Elements

--- d3.js
+++ d3.js
@@ -23,3 +23,14 @@ d3.select("body")
   .enter()
   .append("h2")
   .text((d) => d + " USD");
+
+//Add Inline Styling to Elements
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body")
+  .selectAll("h2")
+  .data(dataset)
+  .enter()
+  .append("h2")
+  .text((d) => d + " USD")
+  .style("font-family", "verdana");
