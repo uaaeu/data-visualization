432ec48f67cca03023ae7e8c5c1538d3a1cd3220
uaaeu <yurtmusa@gmail.com>
2020-07-14 16:20:59 +0300

    Display Shapes with SVG

--- d3.js
+++ d3.js
@@ -85,3 +85,20 @@ const w = 500;
 const h = 100;
 
 const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+//Display Shapes with SVG
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+const w = 500;
+const h = 100;
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  .append("rect")
+  .attr("width", 25)
+  .attr("height", 100)
+  .attr("x", 0)
+  .attr("y", 0);
