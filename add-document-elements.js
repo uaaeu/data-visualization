379f0782e026fbcd3960cd58c5389262a24d32b1
uaaeu <yurtmusa@gmail.com>
2020-07-14 15:48:11 +0300

    Select a Group of Elements with D3

--- add-document-elements.js
+++
@@ -1,2 +0,0 @@
-//Add Document Elements with D3
-d3.select('body').append('h1').text("Learning D3")
---
+++ d3.js
@@ -0,0 +1,5 @@
+//Add Document Elements with D3
+d3.select("body").append("h1").text("Learning D3");
+
+//Select a Group of Elements with D3
+d3.selectAll("li").text("list item");
