15c544710a195564ba58cf76ebef0881bc945c54
uaaeu <yurtmusa@gmail.com>
2020-07-14 21:42:20 +0300

    Handle Click Events with js using the onclick

---
+++ json_api_ajax.js
@@ -0,0 +1,5 @@
+//Handle Click Events with JavaScript using the onclick property
+document.addEventListener("DOMContentLoaded", function () {
+
+  document.getElementById("getMessage").onclick = function () {};
+});
