c54e1a966054227935407cd5e05edcd97831e0b8
uaaeu <yurtmusa@gmail.com>
2020-07-14 16:05:13 +0300

    Add Classes with D3

--- d3.js
+++ d3.js
@@ -45,3 +45,13 @@ d3.select("body")
   .append("h2")
   .text((d) => d + " USD")
   .style("color", (d) => (d < 20 ? "red" : "green"));
+
+//Add Classes with D3
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar");
