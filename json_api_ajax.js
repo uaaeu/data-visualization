05e24b7125a82fb89ada83eb7dcd2ec5dc0e595c
uaaeu <yurtmusa@gmail.com>
2020-07-14 21:47:04 +0300

    Change Text with click Events

--- json_api_ajax.js
+++ json_api_ajax.js
@@ -1,5 +1,9 @@
 //Handle Click Events with JavaScript using the onclick property
 document.addEventListener("DOMContentLoaded", function () {
-
-  document.getElementById("getMessage").onclick = function () {};
+  
+    document.getElementById("getMessage").onclick = function () {
+    
+    //Change Text with click Events
+    document.getElementsByClassName("message")[0].textContent="Here is the message";
+  };
 });
