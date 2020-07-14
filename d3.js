bb96abe4d1377d7fc45c70bebca04ccead016614
uaaeu <yurtmusa@gmail.com>
2020-07-14 16:08:20 +0300

    Update the Height of an Element Dynamically

--- d3.js
+++ d3.js
@@ -55,3 +55,14 @@ d3.select("body")
   .enter()
   .append("div")
   .attr("class", "bar");
+
+//Update the Height of an Element Dynamically
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", (d) => d + "px");
