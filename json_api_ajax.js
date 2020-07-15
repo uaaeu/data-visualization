ee458becb706c38aba1105b0e42d88d0c58461ed
uaaeu <yurtmusa@gmail.com>
2020-07-15 09:47:29 +0300

    Render Images from Data Sources

--- json_api_ajax.js
+++ json_api_ajax.js
@@ -59,3 +59,24 @@ document.addEventListener("DOMContentLoaded", function () {
     };
   };
 });
+
+//Render Images from Data Sources
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("getMessage").onclick = function () {
+    const req = new XMLHttpRequest();
+    req.open("GET", "/json/cats.json", true);
+    req.send();
+    req.onload = function () {
+      const json = JSON.parse(req.responseText);
+      let html = "";
+      json.forEach(function (val) {
+        html += "<div class = 'cat'>";
+        html +=
+          "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>";
+
+        html += "</div><br>";
+      });
+      document.getElementsByClassName("message")[0].innerHTML = html;
+    };
+  };
+});
